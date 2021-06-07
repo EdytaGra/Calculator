cebabd9879c10b604fa7f1745b89d6df9e83ef5a
EdytaGra <edyta.grajcar@gmail.com>
2021-06-07 14:23:07 +0200

    change def_run: while, break

--- calculator.py
+++ calculator.py
@@ -1,86 +1,94 @@
-from typing import Union
 from math import sqrt
 
 
 class Calculator:
 
-    def addition(self) -> Union[int, float]:
-        a = input(f'Give a number: ')
-        b = input(f'Give second number: ')
-        return int(a) + int(b)
+    def addition(self) -> float:
+        a = float(input(f'Give a number: '))
+        b = float(input(f'Give a second number: '))
+        return a + b
 
-    def subtraction(self) -> Union[int, float]:
+    def subtraction(self) -> float:
         a = input(f'Give a number: ')
         b = input(f'Give second number: ')
         return int(a) - int(b)
 
-    def multiplication(self) -> Union[int, float]:
+    def multiplication(self) -> float:
         a = input(f'Give a number: ')
         b = input(f'Give second number: ')
         return int(a) * int(b)
 
-    def division(self) -> Union[int, float]:
+    def division(self) -> float:
         a = input(f'Give a number: ')
         b = input(f'Give second number: ')
         return int(a) / int(b)
 
-    def exponentiation(self) -> Union[int, float]:
+    def exponentiation(self) -> float:
         a = input(f'Give a number: ')
         b = input(f'Give second number: ')
         return int(a) ** int(b)
 
-    def rooting(self) -> Union[int, float]:
+    def rooting(self) -> float:
         a = input(f'Give a number: ')
         return sqrt(int(a))
 
-    def square_area(self) -> Union[int, float]:
+    def square_area(self) -> float:
         a = input(f'Give a number: ')
         return int(a) * int(a)
 
-    def rectangle_area(self) -> Union[int, float]:
+    def rectangle_area(self) -> float:
         a = input('Give a number:')
         b = input('Give second number:')
         return int(a) * int(b)
 
-    def triangle_area(self) -> Union[int, float]:
+    def triangle_area(self) -> float:
         a = input('Give a number:')
         h = input('Give second number:')
-        return int(a * h) / int(2)
+        return int(a) * int(h) / 2
 
-    def circle_area(self) -> Union[int, float]:
+    def circle_area(self) -> float:
         a = input('Give a number:')
-        return float(3.14 * float(a) ** int(2))
+        return 3.14 * float(a) ** 2
+
+    def print_available_functions(self):
+        print('Menu:\n'
+              '1 Addition\n'
+              '2 Subtraction\n'
+              '3 Multiplication\n'
+              '4 Division\n'
+              '5 Exponentiation\n'
+              '6 Rooting\n'
+              '7 Square area\n'
+              '8 Rectangle area\n'
+              '9 Triangle area\n'
+              '10 Circle area\n'
+              '0 Exit')
 
     def run(self):
-        print('Menu :\n'
-              '1 addition\n'
-              '2 subtraction\n'
-              '3 multiplication\n'
-              '4 division\n'
-              '5 exponentiation\n'
-              '6 rooting\n'
-              '7 square area\n'
-              '8 rectangle area\n'
-              '9 triangle area\n'
-              '10 circle area')
-        user_input = input('Choose number: ')
-        if user_input == str(1):
-            print(self.addition())
-        elif user_input == str(2):
-            print(self.subtraction())
-        elif user_input == str(3):
-            print(self.multiplication())
-        elif user_input == str(4):
-            print(self.division())
-        elif user_input == str(5):
-            print(self.exponentiation())
-        elif user_input == str(6):
-            print(self.rooting())
-        elif user_input == str(7):
-            print(self.square_area())
-        elif user_input == str(8):
-            print(self.rectangle_area())
-        elif user_input == str(9):
-            print(self.triangle_area())
-        elif user_input == str(10):
-            print(self.circle_area())
+        while True:
+            self.print_available_functions()
+            user_input = int(input('Choose number (0 - 10): '))
+            if user_input == 1:
+                print(self.addition())
+            elif user_input == 2:
+                print(self.subtraction())
+            elif user_input == 3:
+                print(self.multiplication())
+            elif user_input == 4:
+                print(self.division())
+            elif user_input == 5:
+                print(self.exponentiation())
+            elif user_input == 6:
+                print(self.rooting())
+            elif user_input == 7:
+                print(self.square_area())
+            elif user_input == 8:
+                print(self.rectangle_area())
+            elif user_input == 9:
+                print(self.triangle_area())
+            elif user_input == 10:
+                print(self.circle_area())
+            elif user_input == 0:
+                break
+            else:
+                print('Incorrect option selected')
